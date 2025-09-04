f5567ead945bc69152d10c467a3f14c5a3e4c732
Mainak Mondal <mainakmondal24@gmail.com>
2025-09-05 01:35:58 +0530

    color grid added

---
+++ index.js
@@ -0,0 +1,67 @@
+const greetings = ['Hello', '你好', 'Hola', 'Hallo', 'Giea', 'Ciao', 'Bonjour', 'नमस्ते'];
+const colors = [
+    "#f796ae",   "#6e84ffff", "#a0faa0",   "#f5c377ff", 
+    "#e77bfaff", "#7bd5feff", "#f2ff7cff", "#ff8178ff"  
+]
+
+const greet = document.getElementById("greet");
+const colorGrid = document.querySelector(".grid");
+const colorBoxes = document.querySelectorAll('.grid div');
+const toast = document.getElementById('toast');
+
+let currentIndex = 0;
+let colorIndex = 0;
+let isColorGridHovered = false;
+
+function changeGreeting() {
+    greet.style.opacity = 0;
+
+    setTimeout(() => {
+        greet.textContent = greetings[currentIndex];
+        greet.style.opacity = 1;
+        currentIndex = (currentIndex + 1) % greetings.length;
+    }, 500);
+}
+
+
+function displayColor() {
+    if (isColorGridHovered) {
+        colorBoxes.forEach((box, i) => box.style.backgroundColor = colors[i]);
+    } else {
+        colorBoxes.forEach(box => box.style.backgroundColor = "#444");
+        if (colorIndex > 3) colorBoxes[11 - colorIndex].style.backgroundColor = colors[11 - colorIndex];
+        else colorBoxes[colorIndex].style.backgroundColor = colors[colorIndex];
+        colorIndex = (colorIndex + 1) % colorBoxes.length;
+    }
+}
+
+colorGrid.addEventListener("mouseenter", () => {
+    isColorGridHovered = true;
+    displayColor();
+});
+
+colorGrid.addEventListener("mouseleave", () => {
+    isColorGridHovered = false;
+    displayColor();
+});
+
+colorBoxes.forEach((box, i) => {
+    box.addEventListener("click", () => {
+        navigator.clipboard.writeText(colors[i]).then(() => {
+            toast.textContent = colors[i] + " Copied";
+            toast.style.borderColor = colors[i];
+            toast.style.color = colors[i];
+            toast.classList.remove('hidden');
+            toast.style.animation = 'toast-in 0.5s ease'
+
+            setTimeout(() => {
+                toast.classList.add('hidden');
+            }, 3000);
+        });
+    })
+});
+
+
+displayColor();
+setInterval(displayColor, 4000);
+setInterval(changeGreeting, 4000);
